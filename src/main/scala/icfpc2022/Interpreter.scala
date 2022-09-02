cce1346fb9dc7dc01ed9ffd82927cc046ff7d43e
Joao Azevedo <joao.c.azevedo@gmail.com>
2022-09-02 20:31:20 +0100

    Fix cost function on merge

--- src/main/scala/icfpc2022/Interpreter.scala
+++ src/main/scala/icfpc2022/Interpreter.scala
@@ -270,7 +270,11 @@ object Interpreter {
               val nextBlockId = program.blockId + 1
               val nextBlocks =
                 program.canvas.blocks.removed(blockId1).removed(blockId2).updated(nextBlockId.toString, mergedBlock)
-              val nextCost = program.cost + costOf(move, program.canvas.shape, if (block1.shape.size > block2.shape.size) block1.shape else block2.shape)
+              val nextCost = program.cost + costOf(
+                move,
+                program.canvas.shape,
+                if (block1.shape.size > block2.shape.size) block1.shape else block2.shape
+              )
 
               Right(
                 program.copy(
@@ -290,7 +294,11 @@ object Interpreter {
               val nextBlockId = program.blockId + 1
               val nextBlocks =
                 program.canvas.blocks.removed(blockId1).removed(blockId2).updated(nextBlockId.toString, mergedBlock)
-              val nextCost = program.cost + costOf(move, program.canvas.shape, if (block1.shape.size > block2.shape.size) block1.shape else block2.shape)
+              val nextCost = program.cost + costOf(
+                move,
+                program.canvas.shape,
+                if (block1.shape.size > block2.shape.size) block1.shape else block2.shape
+              )
 
               Right(
                 program.copy(
