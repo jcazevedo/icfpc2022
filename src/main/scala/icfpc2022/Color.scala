9dd7cabe9ee777c49ef0d292e37406d6e9ea5d62
Joao Azevedo <joao.c.azevedo@gmail.com>
2022-09-02 19:57:08 +0100

    Add a `fromInt` method to `Color`

--- src/main/scala/icfpc2022/Color.scala
+++ src/main/scala/icfpc2022/Color.scala
@@ -4,3 +4,8 @@ case class Color(r: Int, g: Int, b: Int, a: Int) {
   def toInt: Int =
     (a << 24) + (r << 16) + (g << 8) + b
 }
+
+object Color {
+  def fromInt(x: Int): Color =
+    Color(r = (x >> 16) & 0xff, g = (x >> 8) & 0xff, b = x & 0xff, a = (x >> 24) & 0xff)
+}
