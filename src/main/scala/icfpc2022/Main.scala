d7eee1667a01a42f44e54ce424094fe54995f301
Joao Azevedo <joao.c.azevedo@gmail.com>
2022-09-02 20:31:30 +0100

    Add initial solver implementation

--- src/main/scala/icfpc2022/Main.scala
+++ src/main/scala/icfpc2022/Main.scala
@@ -12,26 +12,29 @@ object Main extends App {
   // println(image.getWidth())
   // println(image.getRGB(10, 10).toHexString)
 
-  val p = Interpreter.apply(
-    Program(Canvas.blank(400, 400)),
-    List(
-      PointCutMove("0", Coords(100, 100)),
-      ColorMove("0.0", Color(255, 0, 0, 255)),
-      ColorMove("0.1", Color(0, 255, 0, 255)),
-      ColorMove("0.2", Color(0, 0, 255, 255)),
-      ColorMove("0.3", Color(0, 0, 0, 255)),
-      LineCutMove("0.2", LineCutMove.Vertical, 250),
-      ColorMove("0.2.1", Color(255, 0, 0, 255)),
-      SwapMove("0.2.1", "0.2.0"),
-      MergeMove("0.2.1", "0.2.0"),
-      MergeMove("1", "0.3"),
-      ColorMove("2", Color(255, 255, 255, 255))
-    )
-  )
+  // val p = Interpreter.apply(
+  //   Program(Canvas.blank(400, 400)),
+  //   List(
+  //     PointCutMove("0", Coords(100, 100)),
+  //     ColorMove("0.0", Color(255, 0, 0, 255)),
+  //     ColorMove("0.1", Color(0, 255, 0, 255)),
+  //     ColorMove("0.2", Color(0, 0, 255, 255)),
+  //     ColorMove("0.3", Color(0, 0, 0, 255)),
+  //     LineCutMove("0.2", LineCutMove.Vertical, 250),
+  //     ColorMove("0.2.1", Color(255, 0, 0, 255))
+  //   )
+  // )
 
-  println(p.toOption.get.isl)
+  // println(p.toOption.get.isl)
 
-  val image = Interpreter.paint(p.toOption.get)
+  // val image = Interpreter.paint(p.toOption.get)
 
-  ImageIO.write(image, "png", new File("test.png"))
+  // ImageIO.write(image, "png", new File("test.png"))
+
+  (1 to 15).foreach { id =>
+    val (p, cost) = Solver.solve(new File(s"problems/$id.png"))
+    println(s"$id -> $cost")
+    println(p.isl)
+    println()
+  }
 }
